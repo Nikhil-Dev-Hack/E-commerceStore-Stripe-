16b56d615f7d8d0d5ccf7862874e1315bed7cfb1
Nikhil Banuri <75464384+Nikhil-Dev-Hack@users.noreply.github.com>
2022-07-28 15:24:29 +0530

    Removed unwanted usings

--- skinet/Infrastructure/Data/StoreContextSeed.cs
+++ skinet/Infrastructure/Data/StoreContextSeed.cs
@@ -1,11 +1,6 @@
 ﻿using Core.Entities;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
